3ec2598a9f0c38ab145d57341a2a2a14c9e9addc
Mirwais Farahi <waisfarahi@gmail.com>
2022-09-08 23:54:00 +0500

    Edit App.js File

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Calculator />
-    );
-  }
+function App() {
+  return (
+    <Calculator />
+  );
 }
 
 export default App;
